b7456e313e04c2056fa8f55113542c5b3faec112
Grzegorz Grazewicz <grzgra88@gmail.com>
2023-05-06 20:31:37 +0200

    Dodanie odszumiania PCA - potrzebna zmiana danych wejsciowych

--- Physionet_denoising.py
+++ Physionet_denoising.py
@@ -1,6 +1,7 @@
 import wfdb
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.decomposition import PCA
 
 
 def add_noise(signal, amplitude):
@@ -12,19 +13,29 @@ database_path = './ECG1/'
 record = wfdb.rdrecord(database_path + 'Person_01/rec_1')
 annotation = wfdb.rdann(database_path + 'Person_01/rec_1', 'atr')
 
-noisy_signal = add_noise(record.p_signal[:, 0], 0.05)
+signal_noise = add_noise(record.p_signal[:, 0], 0.05)
 
 # print('INFORMACJE O SYGNALE:', record.__dict__)
 # print('INFORMACJE O ADNOTACJACH:', annotation.__dict__)
 
+pca = PCA(n_components=3)
+pca.fit(signal_noise.reshape(-1, 1))
+signal_PCA = pca.inverse_transform(pca.transform(signal_noise.reshape(-1, 1))).flatten()
 
-plt.plot(record.p_signal)
-plt.xlabel('Czas [próbka]')
-plt.ylabel('Napięcie [mV]')
+fig, (p1, p2, p3) = plt.subplots(3, 1, figsize=(10, 8))
+
+p1.plot(record.p_signal)
+p1.set_ylabel('Amplituda')
+p1.set_title('Oryginalny sygnał EKG')
+
+p2.plot(signal_noise)
+p2.set_ylabel('Amplituda')
+p2.set_title('Sygnał EKG + szum')
+
+p3.plot(signal_PCA)
+p3.set_xlabel('Czas [próbka]')
+p3.set_ylabel('Amplituda')
+p3.set_title('Sygnał EKG po PCA()')
+
+plt.tight_layout()
 plt.show()
-
-plt.plot(noisy_signal)
-plt.xlabel('Czas [próbka]')
-plt.ylabel('Napięcie [mV]')
-plt.show()
-
