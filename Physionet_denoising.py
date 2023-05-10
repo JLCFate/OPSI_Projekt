8f1c628ab2681bb187bf128f54c3b085a2dac9ff
Grzegorz Grazewicz <grzgra88@gmail.com>
2023-05-10 22:21:52 +0200

    new database + refactor (PCA, ICA - done)

--- Physionet_denoising.py
+++ Physionet_denoising.py
@@ -5,12 +5,45 @@ from sklearn.decomposition import PCA, FastICA
 import pywt
 
 
-def add_noise(signal, amplitude):
-    noise = np.random.normal(scale=amplitude, size=len(signal))
-    return signal + noise
+def get_data(file_name='Norwegian/ath_001'):
+    database_path = './ECG1/'
+    path = database_path + file_name
+    ref = wfdb.rdrecord(path)
+    rec = wfdb.rdrecord(path)
+
+    return ref, rec
 
 
-def wavelet_denoise(signal, wavelet='db4', level=1):
+def get_info(rec):
+    print('INFORMACJE O SYGNALE:', rec.__dict__)
+
+
+def add_noise(signal, amplitude, ch=0):
+    noise = np.random.normal(scale=amplitude, size=len(signal[:, ch]))
+    signal[:, ch] += noise
+
+    return signal
+
+
+def pca_denoise(signal_clear, signal_with_noise, n):
+    pca = PCA(n_components=n)
+    signal_clear_pca = pca.fit_transform(signal_clear)
+    signal_full_pca = np.hstack([signal_with_noise[:, 1:], signal_clear_pca])
+    pca_final = pca.fit_transform(signal_full_pca)
+
+    return pca_final
+
+
+def ica_denoise(signal_clear, signal_with_noise, n):
+    ica = FastICA(n_components=n)
+    signal_clear_ica = ica.fit_transform(signal_clear)
+    signal_full_ica = np.hstack([signal_with_noise[:, 1:], signal_clear_ica])
+    ica_final = ica.fit_transform(signal_full_ica)
+
+    return ica_final
+
+
+def wavelet_denoise(signal, wavelet='db4', level=2):
 
     values = pywt.wavedec(signal, wavelet, level=level)
 
@@ -20,39 +53,33 @@ def wavelet_denoise(signal, wavelet='db4', level=1):
     return pywt.waverec(values, wavelet)
 
 
-database_path = './ECG1/'
-record = wfdb.rdrecord(database_path + 'Person_01/rec_1')
-annotation = wfdb.rdann(database_path + 'Person_01/rec_1', 'atr')
+def make_plot():
+    p = ['p1', 'p2', 'p3', 'p4', 'p5']
 
-signal_noise = add_noise(record.p_signal[:, 0], 0.05)
+    fig, (p) = plt.subplots(5, 1, figsize=(10, 10))
+    for i in range(len(p)):
+        title = ['Reference ECG Signal', 'Noisy ECG Signal', 'ECG after PCA()', 'ECG after ICA()',
+                 'ECG after Wavelet transform']
+        source = [referral.p_signal, signal_noise, signal_PCA, signal_ICA, signal_wavelet]
+        p[i].plot(source[i][:, 0])
+        p[i].set_ylabel('Amplitude')
+        p[i].set_xlabel('Time')
+        if i == 2:
+            p[i].invert_yaxis()  # vertical flip
+        p[i].set_title(title[i])
+    plt.tight_layout()
+    plt.show()
 
-# print('INFORMACJE O SYGNALE:', record.__dict__)
-# print('INFORMACJE O ADNOTACJACH:', annotation.__dict__)
 
+referral, record = get_data()
+get_info(record)
+signal_noise = add_noise(record.p_signal, 0.25, ch=0)
+signal_ref_ch_up = np.delete(record.p_signal, 0, axis=1)
+signal_PCA = pca_denoise(signal_ref_ch_up, signal_noise, 3)
+signal_ICA = ica_denoise(signal_ref_ch_up, signal_noise, 3)
 
-pca = PCA(n_components=1)
-pca.fit(signal_noise.reshape(-1, 1))
-signal_PCA = pca.inverse_transform(pca.transform(signal_noise.reshape(-1, 1))).flatten()
-
-
-ica = FastICA(n_components=1)
-signal_ICA = ica.fit_transform(signal_noise.reshape(-1, 1)).flatten()
 
 signal_wavelet = wavelet_denoise(signal_noise)
 
-p = ['p1', 'p2', 'p3', 'p4', 'p5']
+make_plot()
 
-fig, (p) = plt.subplots(5, 1, figsize=(10, 10))
-
-
-for i in range(len(p)):
-    title = ['Reference ECG Signal', 'Noisy ECG Signal', 'ECG after PCA()', 'ECG after ICA()', 'ECG after Wavelet transform']
-    source = [record.p_signal, signal_noise, signal_PCA, signal_ICA, signal_wavelet]
-    p[i].plot(source[i])
-    p[i].set_ylabel('Amplitude')
-    p[i].set_xlabel('Time')
-    p[i].set_title(title[i])
-
-
-plt.tight_layout()
-plt.show()
