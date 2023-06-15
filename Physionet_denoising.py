dd87a510f1f3797d3a28edb8d96e7be41629ea9f
Grzegorz Grazewicz <grzgra88@gmail.com>
2023-06-15 13:03:54 +0200

    Final version of code -> Prod ready

--- Physionet_denoising.py
+++ Physionet_denoising.py
@@ -7,6 +7,8 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
 def get_data(file_name='Norwegian/ath_001'):
+    # wczytanie danych z PhysioNet zapisanych w lokalnym folderze
+
     database_path = './ECG1/'
     path = database_path + file_name
     ref = wfdb.rdrecord(path)
@@ -16,24 +18,45 @@ def get_data(file_name='Norwegian/ath_001'):
 
 
 def get_info(rec):
+    # wyświetlanie informacji na temat sygnalu
+    # wykorzystywane glownie podczas wstepnej analizy sygnalu
+
     print('INFORMACJE O SYGNALE:', rec.__dict__)
 
 
 def add_noise(signal, amplitude, ch=0):
+    # dodanie szumu do sygnalu, mozliwosc manipulacji amplituda w celu testowania
+
     noise = np.random.normal(scale=amplitude, size=len(signal[:, ch]))
-    signal[:, ch] = noise
+    signal[:, ch] += noise
 
     return signal
 
 
 def pca_denoise(signal_clear, signal_with_noise, n):
-    # pca = PCA(n_components=n)
-    # signal_clear_pca = pca.fit_transform(signal_clear)
-    # signal_full_pca = np.hstack([signal_with_noise[:, 1:], signal_clear_pca])
-    # pca_final = pca.fit_transform(signal_full_pca)
+    # funkcja realizujaca odszumianie z wykorzystaniem metody PCA
+    # liczba komponentow dobierana wzgledem badanego sygnalu
 
     pca = PCA(n_components=n)
     pca_final = pca.fit_transform(signal_with_noise)
+
+    # wyswietlenie matryk pozwalajacych na ocene sygnalu
+
+    mse = mean_squared_error(signal_with_noise[:, 0], pca_final[:, 0])
+    mae = mean_absolute_error(signal_with_noise[:, 0], pca_final[:, 0])
+    print('MSE PCA: ', mse)
+    print('MAE PCA:', mae)
+
+    return pca_final
+
+
+def pca_denoise_1(signal_clear, signal_with_noise, n):
+    # alternatywne podejscie do wykonania odszumiania z wykorzystaniem metody PCA
+
+    pca = PCA(n_components=n)
+    signal_clear_pca = pca.fit_transform(signal_clear)
+    signal_full_pca = np.hstack([signal_with_noise[:, 1:], signal_clear_pca])
+    pca_final = pca.fit_transform(signal_full_pca)
 
     mse = mean_squared_error(signal_with_noise[:, 0], pca_final[:, 0])
     mae = mean_absolute_error(signal_with_noise[:, 0], pca_final[:, 0])
@@ -44,14 +67,31 @@ def pca_denoise(signal_clear, signal_with_noise, n):
 
 
 def ica_denoise(signal_clear, signal_with_noise, n):
+    # funkcja realizujaca odszumianie z wykorzystaniem metody ICA
+    # parametry dobierane wzgledem badanego sygnalu
+    # whiten='unit-variance' - dodane w celu zniwelowania errora infoirmujacego o przyszlej zmianie domyslenj wartosci
+    # whiten w wykorzystywanej bibliotece
+
+    ica = FastICA(n_components=n, whiten='unit-variance')
+    ica_final = ica.fit_transform(signal_with_noise)
+
+    # wyswietlenie matryk pozwalajacych na ocene sygnalu
+
+    mse = mean_squared_error(signal_with_noise[:, 0], ica_final[:, 0])
+    mae = mean_absolute_error(signal_with_noise[:, 0], ica_final[:, 0])
+    print('MSE ICA: ', mse)
+    print('MAE ICA:', mae)
+
+    return ica_final
+
+
+def ica_denoise_1(signal_clear, signal_with_noise, n):
+    # Alternatywne podejscie do odszumiania cygnaly z wykorzystaniem metody ICA
+
     ica = FastICA(n_components=n, whiten='unit-variance')
     signal_clear_ica = ica.fit_transform(signal_clear)
     signal_full_ica = np.hstack([signal_with_noise[:, 1:], signal_clear_ica])
     ica_final = ica.fit_transform(signal_full_ica)
-
-    # ica = FastICA(n_components=n, whiten='unit-variance')
-    # ica_final = ica.fit_transform(signal_with_noise)
-    # #Zmienic n na 4 i tez jest git
 
     mse = mean_squared_error(signal_with_noise[:, 0], ica_final[:, 0])
     mae = mean_absolute_error(signal_with_noise[:, 0], ica_final[:, 0])
@@ -62,6 +102,10 @@ def ica_denoise(signal_clear, signal_with_noise, n):
 
 
 def wavelet_denoise(signal, wavelet='db6', level=5):
+    # Funkcja realizujaca odszumianie z wykorzystaniem transformaty falkowej
+    # Sygnal testowano na 2 falkach: db4 oraz db6
+    # Zmienna multi odpowiada za modyfikowanie poziomu odszumiania
+
     multi = 0.1
     values = pywt.wavedec(signal, wavelet, level=level)
     threshold = np.median(np.abs(values[-level])) / multi
@@ -72,6 +116,10 @@ def wavelet_denoise(signal, wavelet='db6', level=5):
 
 
 def make_plot():
+    # Funkcja stworzona na potrzeby wizualizacji danych oraz pozniejszych wynikow
+    # Tworzene jest 5 wykresow odpowiadajacych kolejno czytemu sygnalowi
+    # sygnalowi zaszumionemu oraz wynikach odszumiania
+
     fig, (p) = plt.subplots(5, 1, figsize=(10, 10))
     for i in range(len(p)):
         title = ['Reference ECG Signal', 'Noisy ECG Signal', 'ECG after PCA()', 'ECG after ICA()',
@@ -80,20 +128,22 @@ def make_plot():
         p[i].plot(source[i][:, 0])
         p[i].set_ylabel('Amplitude')
         p[i].set_xlabel('Time')
-        if i == 2:
-            p[i].invert_yaxis()  # vertical flip
         p[i].set_title(title[i])
     plt.tight_layout()
     plt.show()
 
 
+# wywolanie wczesniej stworzonych funkcji oraz podanie im odpowiednich zmiennych
+# sygnal wejsciowy zostal sklonowany w celu latwiejszej interpretacji w kodzie
+
 referral, record = get_data()
-# get_info(record)
-signal_noise = add_noise(record.p_signal, 0.2, ch=0)
+signal_noise = add_noise(record.p_signal, 0.08, ch=0)
 signal_ref_ch_up = np.delete(record.p_signal, 0, axis=1)
-signal_PCA = pca_denoise(signal_ref_ch_up, signal_noise, 3)
+signal_PCA = pca_denoise(signal_ref_ch_up, signal_noise, 4)
 signal_ICA = ica_denoise(signal_ref_ch_up, signal_noise, 3)
 signal_wavelet = wavelet_denoise(signal_noise)
+
+# Dodatkowe wyswietlenie informascji o sygnale po wykonaniu na nim odszumiania z wykorzystaniem transformaty falkowej
 
 mse3 = mean_squared_error(signal_noise[:, 0], signal_wavelet[:, 0])
 mae3 = mean_absolute_error(signal_noise[:, 0], signal_wavelet[:, 0])
@@ -102,9 +152,4 @@ print('MAE wavelet:', mae3)
 
 make_plot()
 
-# plt.plot(signal_ICA)
-# # plt.set_ylabel('Amplitude')
-# # plt.set_xlabel('Time')
-# plt.suptitle("ECG after Wavelet transform")
-# plt.tight_layout()
-# plt.show()
+# Repozytorium projektu dostepne na serwisie GitHub : https://github.com/JLCFate/OPSI_Projekt
